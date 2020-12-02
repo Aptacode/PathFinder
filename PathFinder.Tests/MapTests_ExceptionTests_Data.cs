b887835d62218e43069239d458165e334c9777f5
Electricschmidt <tomdschmidt@gmail.com>
2020-12-02 18:05:15 +0000

    Fixed Map Constructor test

--- PathFinder.Tests/MapTests_ExceptionTests_Data.cs
+++ PathFinder.Tests/MapTests_ExceptionTests_Data.cs
@@ -1,33 +1,44 @@
+using Aptacode.PathFinder.Geometry;
+using Aptacode.PathFinder.Resources;
+using PathFinder.Tests.Helpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+
 namespace PathFinder.Tests
 {
-    //public class MapTest_ExceptionTests_Data : IEnumerable<object[]>
-    //{
-    //    private readonly List<object[]> _data = new List<object[]>
-    //    {
-    //        new object[]
-    //        {
-    //            new Vector2(10, 10), new Vector2(-1, 0), new Vector2(5, 5)
-    //        },
-    //        new object[]
-    //        {
-    //            Map_Helpers.EndPoint_OutOfBounds_Map,
-    //            Resources.END_POINT_OUT_OF_BOUNDS
-    //        },
-    //        new object[]
-    //        {
-    //            Map_Helpers.StartPoint_HasCollision_WithObstacle_Map,
-    //            Resources.START_POINT_HAS_COLLISION_WITH_OBSTACLE
-    //        },
-    //        new object[]
-    //        {
-    //            Map_Helpers.EndPoint_HasCollision_WithObstacle_Map,
-    //            Resources.END_POINT_HAS_COLLISION_WITH_OBSTACLE
-    //        }
-    //    };
+    public class MapTests_ExceptionTests_Data : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[]
+            {
+                ExceptionMessages.StartPointOutOfBounds,
+                new Vector2(10, 10), new Vector2(-1, 0), new Vector2(5, 5)
+            },
+            new object[]
+            {
+                 ExceptionMessages.EndPointOutOfBounds,
+                new Vector2(10, 10), new Vector2(5, 0), new Vector2(-5, 5)
+            },
+            new object[]
+            {
+                ExceptionMessages.StartPointHasCollisionWithObstacle,
+                new Vector2(10, 10), new Vector2(1, 1), new Vector2(5, 5),
+                new Obstacle(Guid.NewGuid(), new Vector2(1, 1), new Vector2(0, 0))
+                
+            },
+            new object[]
+            {
+                ExceptionMessages.EndPointHasCollisionWithObstacle,
+                new Vector2(10, 10), new Vector2(1, 1), new Vector2(5, 5),
+                new Obstacle(Guid.NewGuid(), new Vector2(5, 5), new Vector2(0, 0))               
+            }
+        };
 
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
 
-    //    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
-
-    //    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    //}
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 }
