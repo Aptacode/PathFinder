beed64fa8f8f706e2cb6c17226070b097e89451e
Timothy Jones <t.jones@aptacode.com>
2020-12-27 14:17:01 +0000

    Fixed Blazor demo

--- PathFinder.BlazorDemo/Pages/Index.razor.cs
+++ PathFinder.BlazorDemo/Pages/Index.razor.cs
@@ -1,63 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
-using Aptacode.FlowDesigner.Core.ViewModels;
+using Aptacode.Geometry.Blazor.Components.ViewModels;
+using Aptacode.Geometry.Blazor.Utilities;
+using Aptacode.Geometry.Primitives;
+using Aptacode.Geometry.Vertices;
 using Aptacode.PathFinder.Geometry.Neighbours;
 using Aptacode.PathFinder.Maps;
 using Microsoft.AspNetCore.Components;
+using Rectangle = Aptacode.Geometry.Primitives.Polygons.Rectangle;
 
 namespace PathFinder.BlazorDemo.Pages
 {
     public class IndexBase : ComponentBase
     {
-        public DesignerViewModel Designer { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            var rng = new Random();
-
-            var mapBuilder = new MapBuilder();
-            var dimensions = new Vector2(100, 100);
-            var start = new Vector2(99, 1);
-            var end = new Vector2(1, 99);
-
-            mapBuilder.SetDimensions(dimensions).SetStart(start).SetEnd(end);
-
-            foreach (var i in Enumerable.Range(0, (int) dimensions.X - 1))
-            {
-                foreach (var j in Enumerable.Range(0, (int) dimensions.Y - 1))
-                {
-                    var randomNum = rng.Next(0, 100);
-                    if (randomNum < 30 && (i != start.X && j != start.Y || i != end.X && j != end.Y))
-                    {
-                        mapBuilder.AddObstacle(new Vector2(i, j), new Vector2(0, 0));
-                    }
-                }
-            }
-
-            var map = mapBuilder.Build().Map;
+            var componentBuilder = new ComponentBuilder();
+            var components = new List<ComponentViewModel>();
+            var map = GenerateVerticalBars();
+            var scale = new Vector2(10, 10);
 
             var timer = new Stopwatch();
             timer.Start();
             var path = new Aptacode.PathFinder.Algorithm.PathFinder(map,
-                DefaultNeighbourFinder.Straight(1f)).FindPath().ToList();
+                DefaultNeighbourFinder.Straight(0.5f)).FindPath().ToList();
             timer.Stop();
 
-            var totalLength = path.Zip(path.Skip(1), (a, b) => a - b).Select(s => s.Length()).Sum();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            foreach (var obstacle in map.Obstacles)
+            {
+                obstacle.Scale(scale);
+                components.Add(componentBuilder.SetPrimitive(obstacle).SetFillColor(Color.Red).Build());
+            }
 
-            Designer = new DesignerViewModel((int) map.Dimensions.X, (int) map.Dimensions.Y);
+            var polyLinePath = new PolyLine(VertexArray.Create(path.ToArray()));
+            polyLinePath.Scale(scale);
 
-            //foreach (var obstacle in map.Obstacles)
-            //{
-            //    var newPoint = Designer.AddPoint(obstacle.Position);
-            //    newPoint.FillColor = Color.Red;
-            //}
-
-            //Designer.AddPath(new Aptacode.FlowDesigner.Core.ViewModels.Components.PathViewModel(Guid.NewGuid(), path));
+            components.Add(componentBuilder.SetPrimitive(polyLinePath).Build());
+            SceneController = new PathFinderSceneController(map.Dimensions * scale, components);
 
             await base.OnInitializedAsync();
         }
+
+
+        public Map GenerateVerticalBars()
+        {
+            var mapBuilder = new MapBuilder();
+            const int width = 100;
+            const int height = 100;
+            const int thickness = 5;
+            mapBuilder.SetDimensions(width, height);
+            mapBuilder.SetStart(0, 0);
+            mapBuilder.SetEnd(width, height);
+
+            var count = 0;
+            for (var i = thickness; i < width - thickness; i += 2 * thickness)
+            {
+                var offset = count++ % 2 == 0 ? thickness : -thickness;
+                mapBuilder.AddObstacle(Rectangle.Create(new Vector2(i, offset),
+                    new Vector2(thickness, height - thickness)));
+            }
+
+            var mapResult = mapBuilder.Build();
+            return mapResult.Map;
+        }
+
+        #region Properties
+
+        public PathFinderSceneController SceneController { get; set; }
+        private readonly Random _rand = new();
+
+        #endregion
     }
 }
---
+++ PathFinder.BlazorDemo/Pages/PathFinderSceneController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using Aptacode.Geometry.Blazor.Components.ViewModels;
+using Aptacode.Geometry.Blazor.Extensions;
+
+namespace PathFinder.BlazorDemo.Pages
+{
+    public class PathFinderSceneController : SceneControllerViewModel
+    {
+        private DateTime lastTick = DateTime.Now;
+
+        public PathFinderSceneController(Vector2 size, IEnumerable<ComponentViewModel> components) : base(
+            new SceneViewModel(
+                size,
+                components
+            ))
+        {
+            UserInteractionController.OnMouseDown += UserInteractionControllerOnOnMouseDown;
+            UserInteractionController.OnMouseUp += UserInteractionControllerOnOnMouseUp;
+            UserInteractionController.OnMouseMoved += UserInteractionControllerOnOnMouseMoved;
+        }
+
+        public bool Running { get; set; }
+
+        public ComponentViewModel SelectedComponent { get; set; }
+
+        public override async Task Tick()
+        {
+            var currentTime = DateTime.Now;
+            var delta = currentTime - lastTick;
+            lastTick = currentTime;
+            await base.Tick();
+        }
+
+        private void UserInteractionControllerOnOnMouseMoved(object? sender, Vector2 e)
+        {
+            if (SelectedComponent == null)
+            {
+                return;
+            }
+
+            var delta = e - UserInteractionController.LastMousePosition;
+
+            Translate(SelectedComponent, delta, new List<ComponentViewModel> {SelectedComponent},
+                new CancellationTokenSource());
+        }
+
+        private void UserInteractionControllerOnOnMouseUp(object? sender, Vector2 e)
+        {
+            foreach (var componentViewModel in Scene.Components)
+            {
+                componentViewModel.BorderColor = Color.Black;
+            }
+
+            SelectedComponent = null;
+        }
+
+        private void UserInteractionControllerOnOnMouseDown(object? sender, Vector2 e)
+        {
+            SelectedComponent = null;
+
+            foreach (var componentViewModel in Scene.Components.CollidingWith(e, CollisionDetector))
+            {
+                SelectedComponent = componentViewModel;
+                componentViewModel.BorderColor = Color.Green;
+            }
+
+            Scene.BringToFront(SelectedComponent);
+        }
+    }
+}
